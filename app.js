058db3eb3c28a1d784c6a984d010351ea2344b04
sailakshmy <sailakshmy94@gmail.com>
2022-01-19 10:19:04 +0530

    Commit-20:- Added a button to close the commands list popup, Added commands for time, weather,network, battery and shut down, modified the getWeatherdetails to update the UI only if the location is the same as the user's location, Created battery,network and time status statements, Added Uppercase transcript checks, Added onclick for the close button in the commands popup

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ const battery = document.querySelector('#battery');
 const startEdithButton = document.querySelector("#start_edith");
 const commandsContainer = document.querySelector('.commands');
 const messages = document.querySelector('.messages');
+const closeCommandsList = document.querySelector('#CloseCommandsButton');
 
 // Format time
-const formatTime = (time) =>{
-    if(time<10)
-    return `0${time}`
+const formatTime = (time) => {
+    if (time < 10)
+        return `0${time}`
     else return time;
 }
 
@@ -53,8 +54,13 @@ commands.push('edit my profile');
 commands.push('show me your commands');
 commands.push('what are the commands that you can interpret?');
 commands.push('close your commands list');
-commands.push('Give me the weather details of ___________');
+commands.push('Give me the weather details in ___________');
+commands.push("What's the time?");
+commands.push("What's my battery status?");
+commands.push("What's my network status?");
 commands.push('close tabs');
+commands.push('Shut down');
+commands.push('Take a nap');
 
 const commandList = document.createElement('ul');
 commands.forEach(command => {
@@ -145,39 +151,55 @@ const getWeatherDetails = (location) => {
     xmlHttpRequest.onload = function () {
         if (this.status === 200) {
             const data = JSON.parse(this.responseText);
-            weatherContainer[0].textContent = `Location: ${data.name}`;
-            weatherContainer[1].textContent = `Country: ${data.sys.country}`;
-            weatherContainer[2].textContent = `Weather Type: ${data.weather[0].main}`;
-            weatherContainer[3].textContent = `Description: ${data.weather[0].description}`;
-            weatherContainer[4].src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-            const tempTag = document.createElement("sup");
-            const degreeSymbol = document.createTextNode('o');
-            tempTag.appendChild(degreeSymbol);
-            const celsiusSymbol = document.createTextNode('C');
-            weatherContainer[5].textContent = `Current Temperature: ${celsius(data.main.temp)}`;
-            weatherContainer[5].appendChild(tempTag);
-            weatherContainer[5].appendChild(celsiusSymbol);
-            weatherContainer[6].textContent = `Feels Like: ${celsius(data.main.feels_like)}`;
-            weatherContainer[6].appendChild(tempTag);
-            weatherContainer[6].appendChild(celsiusSymbol);
-            weatherContainer[7].textContent = `Min Temperature: ${celsius(data.main.temp_min)}`;
-            weatherContainer[7].appendChild(tempTag);
-            weatherContainer[7].appendChild(celsiusSymbol);
-            weatherContainer[8].textContent = `Max Temperature: ${celsius(data.main.temp_max)}`;
-            weatherContainer[8].appendChild(tempTag);
-            weatherContainer[8].appendChild(celsiusSymbol);
-            weatherContainer[9].textContent = `Humidity: ${data.main.humidity}%`;
             weatherStatement = `Sai, the weather in ${location} is ${data.weather[0].description}. The current temperature is ${celsius(data.main.temp)} degrees Celsius, but it feels like ${celsius(data.main.feels_like)} degrees Celsius. 
             You can expect a maximum temperature of ${celsius(data.main.temp_max)} degrees Celsius today and the minimum temperatures can fall down to ${celsius(data.main.temp_min)} degrees Celsius.`
+            // The UI will be updated only if the  location is the same as the user's location.
+            if (location === userInfo.location) {
+                weatherContainer[0].textContent = `Location: ${data.name}`;
+                weatherContainer[1].textContent = `Country: ${data.sys.country}`;
+                weatherContainer[2].textContent = `Weather Type: ${data.weather[0].main}`;
+                weatherContainer[3].textContent = `Description: ${data.weather[0].description}`;
+                weatherContainer[4].src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+                const tempTag = document.createElement("sup");
+                const degreeSymbol = document.createTextNode('o');
+                tempTag.appendChild(degreeSymbol);
+                const celsiusSymbol = document.createTextNode('C');
+                weatherContainer[5].textContent = `Current Temperature: ${celsius(data.main.temp)}`;
+                weatherContainer[5].appendChild(tempTag);
+                weatherContainer[5].appendChild(celsiusSymbol);
+                weatherContainer[6].textContent = `Feels Like: ${celsius(data.main.feels_like)}`;
+                weatherContainer[6].appendChild(tempTag);
+                weatherContainer[6].appendChild(celsiusSymbol);
+                weatherContainer[7].textContent = `Min Temperature: ${celsius(data.main.temp_min)}`;
+                weatherContainer[7].appendChild(tempTag);
+                weatherContainer[7].appendChild(celsiusSymbol);
+                weatherContainer[8].textContent = `Max Temperature: ${celsius(data.main.temp_max)}`;
+                weatherContainer[8].appendChild(tempTag);
+                weatherContainer[8].appendChild(celsiusSymbol);
+                weatherContainer[9].textContent = `Humidity: ${data.main.humidity}%`;
+            }
+            else {
+                readOut(weatherStatement);
+            }
+            // console.log(weatherStatement)
         }
         else {
             weatherContainer[0].textContent = `Weather Info Not Found`;
             weatherStatement = `Sorry Sai. I am unable to retrieve the weather information for ${location}. Please try again later.`
+            readOut(weatherStatement);
         }
-        // readOut(weatherStatement);
     };
     xmlHttpRequest.send()
 }
+
+//Time Statement
+let timeStatement = '';
+
+// Battery Statement
+let batteryStatement = '';
+
+// Network Statement
+let networkStatement = '';
 
 // Message playback/display on screen
 const messageDisplay = (speaker, msg) => {
@@ -208,34 +230,34 @@ recognition.onresult = (event) => {
     if (transcript.includes('hi') || transcript.includes("hey") || transcript.includes('hello')) {
         readOut(`Hello ${edithUserSetup.querySelector('#Nickname').value ? edithUserSetup.querySelector('#Nickname').value : ''}. How can I help you today?`);
     }
-    if (transcript.includes('open YouTube')) {
+    if (transcript.includes('open YouTube') || transcript.includes('Open YouTube')) {
         console.log(`You said ${transcript}`);
         readOut("Opening Youtube");
         const openedTab = window.open('https://www.youtube.com');
         browserTabs.push(openedTab);
     }
-    if (transcript.includes('open Google')) {
+    if (transcript.includes('open Google') || transcript.includes('Open Google')) {
         console.log(`You said ${transcript}`);
         readOut("Opening Google");
         const tab = window.open('https://www.google.com');
         browserTabs.push(tab);
     }
     if (transcript.includes("Amazon")) {
-        if (transcript.includes('open my Amazon')) {
+        if (transcript.includes('open my Amazon') || transcript.includes('Open my Amazon')) {
             console.log(`You said ${transcript}`);
             readOut("Opening your amazon account");
             const tab = window.open('https://www.amazon.in');
             browserTabs.push(tab);
 
         }
-        else if (transcript.includes('open Amazon music')) {
+        else if (transcript.includes('open Amazon music') || transcript.includes('Open Amazon music')) {
             console.log(`You said ${transcript}`);
             readOut("Opening your Amazon Music");
             const tab = window.open('https://music.amazon.in/');
             browserTabs.push(tab);
 
         }
-        else if (transcript.includes('open Amazon Prime video')) {
+        else if (transcript.includes('open Amazon Prime video') || transcript.includes('Open Amazon Prime video')) {
             console.log(`You said ${transcript}`);
             readOut("Opening your Amazon Prime Video");
             const tab = window.open('https://www.primevideo.com/');
@@ -250,64 +272,64 @@ recognition.onresult = (event) => {
 
         }
     }
-    if (transcript.includes('open my Netflix')) {
+    if (transcript.includes('open my Netflix') || transcript.includes('Open my Netflix')) {
         console.log(`You said ${transcript}`);
         readOut("Opening your Netflix account");
         const tab = window.open('https://www.netflix.com/browse');
         browserTabs.push(tab);
 
     }
-    if (transcript.includes('open my GitHub')) {
+    if (transcript.includes('open my GitHub') || transcript.includes('Open my GitHub')) {
         console.log(`You said ${transcript}`);
         readOut("Opening your Github account");
         const tab = window.open(`${edithUserSetup.querySelector('#githubProfile').value}`);
         browserTabs.push(tab);
 
     }
-    if (transcript.includes('open my portfolio')) {
+    if (transcript.includes('open my portfolio') || transcript.includes('Open my portfolio')) {
         console.log(`You said ${transcript}`);
         readOut("Opening your portfolio");
         const tab = window.open(`${edithUserSetup.querySelector('#portfolio').value}`);
         browserTabs.push(tab);
 
     }
-    if (transcript.includes('open my Linkedin profile')) {
+    if (transcript.includes('open my Linkedin profile') || transcript.includes('Open my Linkedin profile')) {
         console.log(`You said ${transcript}`);
         readOut("Opening your LinkedIn profile");
         const tab = window.open(`${edithUserSetup.querySelector('#LinkedInProfile').value}`);
         browserTabs.push(tab);
 
     }
-    if (transcript.includes('open my official Gmail inbox')) {
+    if (transcript.includes('open my official Gmail inbox') || transcript.includes('Open my official Gmail inbox')) {
         console.log(`You said ${transcript}`);
         readOut("Opening your official gmail inbox");
         const tab = window.open('https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox');
         browserTabs.push(tab);
 
     }
-    if (transcript.includes('open my other Gmail inbox')) {
+    if (transcript.includes('open my other Gmail inbox') || transcript.includes('Open my other Gmail inbox')) {
         console.log(`You said ${transcript}`);
         readOut("Opening leftme94 gmail inbox");
         const tab = window.open('https://mail.google.com/mail/u/1/?ogbl#inbox');
         browserTabs.push(tab);
 
     }
-    if (transcript.includes('open my firebase console')) {
+    if (transcript.includes('open my firebase console') || transcript.includes('Open my firebase console')) {
         console.log(`You said ${transcript}`);
         readOut("Opening your firebase console");
         const tab = window.open('https://console.firebase.google.com/u/1/');
         browserTabs.push(tab);
 
     }
-    if (transcript.includes('search for') || transcript.includes('what is')) {
+    if (transcript.includes('search for') || transcript.includes('what is') || transcript.includes('Search for') || transcript.includes('What is')) {
         console.log(`You said ${transcript}`);
         let searchKeyword = '';
         let searchKeywordArray = [];
-        if (transcript.includes('search for') && !transcript.includes('YouTube')) {
+        if ((transcript.includes('search for') || transcript.includes('Search for')) && !transcript.includes('YouTube')) {
             searchKeywordArray = transcript.split(' ').splice((transcript.indexOf('search') + 2));
             searchKeyword = searchKeywordArray.join("+");
         }
-        else if (transcript.includes('what is')) {
+        else if (transcript.includes('what is') || transcript.includes('What is')) {
             searchKeywordArray = transcript.split(' ').splice((transcript.indexOf('what') + 2));
             searchKeyword = searchKeywordArray.join("+");
         }
@@ -324,7 +346,6 @@ recognition.onresult = (event) => {
             readOut(`Opening search results for ${searchKeywordArray.join(" ")}`);
             const tab = window.open(`https://www.google.com/search?q=${searchKeyword}`);
             browserTabs.push(tab);
-
         }
     }
     if (transcript.includes('my profile')) {
@@ -361,21 +382,39 @@ recognition.onresult = (event) => {
             readOut(weatherStatement);
         else {
             const locationSearchKeyword = transcript.split(" ").pop();
-            console.log(typeof locationSearchKeyword);
-            if (locationSearchKeyword.endsWith('?') || locationSearchKeyword.endsWith('.'))
+            console.log(locationSearchKeyword);
+            if (locationSearchKeyword.endsWith('?') || locationSearchKeyword.endsWith('.')) {
                 getWeatherDetails(locationSearchKeyword.split("").splice(0, (locationSearchKeyword.length) - 1).join(""));
-            readOut(weatherStatement);
+            }
+            else {
+                getWeatherDetails(locationSearchKeyword)
+            }
+            // readOut(weatherStatement);
         }
     }
-    if (transcript.includes('close tabs') || transcript.includes('close tab')) {
+    if (transcript.includes('close tabs') || transcript.includes('close tab') || transcript.includes('Close tabs') || transcript.includes('close all tabs')) {
         if (browserTabs.length > 0) {
-            readOut('Closing all tabs');
-            browserTabs.forEach(tab => window.close());
+            readOut('Closing all tabs that I have opened');
+            browserTabs.forEach(window => window.close());
         } else {
             readOut('Sorry, I have not opened any new tabs for you.');
         }
 
     }
+    if (transcript.includes('time')) {
+        readOut(timeStatement);
+    }
+    if (transcript.includes('battery')) {
+        readOut(batteryStatement);
+    }
+    if(transcript.includes('network')) {
+        readOut(networkStatement);
+    }
+    if(transcript.includes('shut down') || transcript.includes('Shut down') || transcript.includes('shutdown') || transcript.includes('take a nap') || transcript.includes('Take a nap')){
+        readOut('Okay, I will take a nap.');
+        recognition.stop();
+    }
+    // console.log(browserTabs);
 }
 
 
@@ -391,6 +430,10 @@ recognition.continuous = true;
 // startButton.addEventListener('click', () => { recognition.start() });
 // stopButton.addEventListener('click', () => { recognition.stop() });
 startEdithButton.addEventListener('click', () => { recognition.start() });
+closeCommandsList.addEventListener('click',() => {
+    commandsContainer.style.display = 'none';
+    commandsContainer.removeChild();
+});
 
 //EDITH's Speech
 const readOut = (message) => {
@@ -420,7 +463,6 @@ window.onload = () => {
             bootUp.addEventListener("onend", () => {
             });
             readOut('Accessing your location');
-            console.log(getUserLocation());
             getUserLocation();
         }, 11000);
     } else if (localStorage.getItem('edith_setup') === null) {
@@ -434,8 +476,10 @@ window.onload = () => {
             const batteryLevel = Math.floor(batteryDetails.level * 100);
             battery.style.width = batteryDetails.charging ? "175px" : "125px";
             battery.textContent = `${batteryLevel}% ${batteryDetails.charging ? " - Charging" : ''}`;
+            batteryStatement = `${batteryDetails.charging ? `You are connected to a power source and have ${batteryLevel} % battery currently.`:`You have ${batteryLevel}% battery left`}`;
             network.textContent = `${navigator.onLine ? 'Online' : 'Offline'}`;
-            if (batteryLevel <= 20 && !batteryDetails.charging) {
+            networkStatement = `${navigator.onLine ? 'You are currently online' : ' Sorry. looks like you are not connected to any network'}`;
+            if (batteryLevel <= 25 && !batteryDetails.charging) {
                 readOut('DamnIt! Looks like I am going to run out of energy soon. Please connect me to a power source.');
             }
         }, 11000);
@@ -447,16 +491,13 @@ window.onload = () => {
     const hrs = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    const day = date.getUTCDay();
     time.textContent = `${formatTime(hrs)}:${formatTime(minutes)}:${formatTime(seconds)}`;
+    timeStatement = `It is currently ${hrs} hours and ${minutes}minutes`;
     // Displays the updated time every 1000 ms
     setInterval(() => {
         const updatedDate = new Date();
-    //         const hrs = date.getHours();
-    // const minutes = date.getMinutes();
-    // const seconds = date.getSeconds();
-    // const day = date.getUTCDay();
         time.textContent = `${formatTime(updatedDate.getHours())}:${formatTime(updatedDate.getMinutes())}:${formatTime(updatedDate.getSeconds())}`;
+        timeStatement = `It is currently ${updatedDate.getHours()} hours and ${updatedDate.getMinutes()}minutes`;
     }, 1000);
 
 };
